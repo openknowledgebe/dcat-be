1094711bd01bd2157501fb67ff55e800ab858569
Sander Meert <satanderm@gmail.com>
2015-04-16 16:22:43 +0200

    First (extremely rough) version of the website

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+    $( '.scroll' ).on('click', function(event) {
+        event.preventDefault();
+        var target = "#" + $(this).data('target');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top - 80
+        }, 700);
+    });
+
+    $( '.scrollTop' ).on('click', function(event) {
+        event.preventDefault();
+        $('body').animate({
+            scrollTop: $('body').offset().top
+        }, 700);
+    });
+
+    if(!Modernizr.svg) {
+        $('img[src*="svg"]').attr('src', function() {
+            return $(this).attr('src').replace('.svg', '.png');
+        });
+    }
+
+});
