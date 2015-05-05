7e8d5781711200f59e98ae010976c44cfb5bc4df
Sander Meert <satanderm@gmail.com>
2015-05-05 11:41:09 +0200

    #2 Meetings organised in JSON format and dynamically loaded, #4 Partner logos added with links, #5 Added 5 live demos of existing DCAT feeds, updated content, added Twitter card/FB OG

--- js/main.js
+++ js/main.js
@@ -1,18 +1,13 @@
 $(function(){
+
+    var weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
     init();
 
     function init() {
         $('#live-example').hide();
         loadMeetings();
     }
-
-    $( '.scroll' ).on('click', function(event) {
-        event.preventDefault();
-        var target = "#" + $(this).data('target');
-        $('html, body').animate({
-            scrollTop: $(target).offset().top - 80
-        }, 700);
-    });
 
     $( '.scrollTop' ).on('click', function(event) {
         event.preventDefault();
@@ -30,24 +25,24 @@ $(function(){
     $( '.exampleDcat' ).on('click', function(event) {
         event.preventDefault();
         var that = $(this);
-        that.next("i").removeClass("hidden");
+        that.next('i').removeClass('hidden');
         getExample(that);
     });
 
     function getExample(that) {
-        var url = that.attr("href");
+        var url = that.attr('href');
         $.ajax({
             url: url,
             error: function() {
                 console.log('An error has occurred');
-                that.next("i").addClass("hidden");
+                that.next('i').addClass('hidden');
             },
             success: function(data) {
                 showExample(data);
                 $('#live-example-title').html(that.html());
                 $('#live-example-footer a').attr('href', that.attr('href'));
                 $('#live-example').show();
-                that.next("i").addClass("hidden");
+                that.next('i').addClass('hidden');
             },
             type: 'GET',
             cache: true
@@ -91,31 +86,56 @@ $(function(){
     }
 
     function parseMeetings(data) {
-        var weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
         $.each(data.upcoming, function(i, v) {
-           // $("#upcoming-meeting");
-            $("#upcoming-meeting-desc").html(v.description);
-            var date = new Date(v.startDate);
-            var enddate = new Date(v.endDate);
-            var dateString = weekday[date.getDay()] + ', ' +
-                date.getDate() + '/' + date.getMonth() +
-                '/' + date.getFullYear() + ' - ' +
-                date.getHours() + ':' + date.getMinutes() + ' - ' +
-                enddate.getHours() + ':' + enddate.getMinutes();
-            var t = $(
-                '<a href="' + v.link + '" class="list-group-item">' +
-                'Open Data in Belgium' +
-                '<br>' + dateString +
-                '<i class="fa fa-chevron-right arrow-right"></i></a>'
-            );
-            $("#upcoming-meeting").html(t);
+
+            if(v.description && v.description.length > 0) {
+                $('#upcoming-meeting-desc').html(v.description);
+            } else {
+                $('#upcoming-meeting-desc').html('');
+            }
+
+            if(!v.startDate || !v.name || !v.link) {
+                $('#upcoming-meeting').html('');
+                $('#upcoming-meeting-desc').html('Currently there are no meetings planned in the near future.');
+            } else {
+                var t = parseMeeting(v);
+                $('#upcoming-meeting').html(t);
+            }
         });
 
         $.each(data.past, function(i, v) {
-            console.log("past");
+            var t = parseMeeting(v);
+            $('#past-meetings').append(t);
         });
+    }
 
+    function parseMeeting(m) {
+        var meeting;
 
+        var date = new Date(m.startDate);
+        var enddate = new Date(m.endDate);
+
+        var minutes = ('0'+date.getMinutes()).slice(-2);
+        var endMinutes = ('0'+enddate.getMinutes()).slice(-2);
+        var day = ('0'+date.getDate()).slice(-2);
+        var month = ('0'+(date.getMonth()+1)).slice(-2);
+        var hours = ('0'+date.getHours()).slice(-2);
+        var endHours = ('0'+enddate.getHours()).slice(-2);
+
+        var dateString = weekday[date.getDay()] + ', ' +
+            day + '/' + month +
+            '/' + date.getFullYear() + ' - ' +
+            hours + ':' + minutes + ' - ' +
+            endHours + ':' + endMinutes;
+
+        meeting = $(
+            '<a href="' + m.link + '" class="list-group-item">' +
+            m.name +
+            '<br>' + dateString +
+            '<i class="fa fa-chevron-right arrow-right"></i></a>'
+        );
+
+        return meeting;
     }
 });
